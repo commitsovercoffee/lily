658261c40823abd337ac333e7f0c5f98b7ad7617
commitsovercoffee <commitsovercoffee@gmail.com>
2023-08-02 01:00:22 +0530

    adds snapping to draggable component

--- src/routes/components/Draggable.svelte
+++ src/routes/components/Draggable.svelte
@@ -2,6 +2,10 @@
 	export let left = 100;
 	export let top = 100;
 
+	const cardStyle =
+		"cursor-move select-none p-8 bg-red-300 h-8 w-16 ease-linear absolute";
+
+	let isCardInsideContainer = false;
 	// vars binded to elements
 	let container;
 	let card;
@@ -30,44 +34,50 @@
 			top = e.clientY - offsetY;
 
 			console.log(checkOverlap(e));
-
-			// Check if cardRect is inside containerRect
-			const isCardInsideContainer =
-				cardRect.left >= containerRect.left &&
-				cardRect.top >= containerRect.top &&
-				cardRect.right <= containerRect.right &&
-				cardRect.bottom <= containerRect.bottom;
-
-			console.log(
-				"Is card inside container?",
-				isCardInsideContainer
-			);
-
-			// write code to snap card vertical center of container
-			if (isCardInsideContainer) {
-				const containerCenterY =
-					containerRect.top +
-					containerRect.height / 2;
-				const cardCenterY =
-					cardRect.top + cardRect.height / 2;
-				const offsetYCenter =
-					containerCenterY - cardCenterY;
-				top += offsetYCenter;
-			}
 		}
 	}
 
 	function onMouseUp() {
 		isDragging = false;
+
+		// Check if cardRect is inside containerRect
+		isCardInsideContainer =
+			// top
+			cardRect.top >= containerRect.top - cardRect.height &&
+			cardRect.top <=
+				containerRect.bottom + cardRect.height &&
+			// left
+			cardRect.left >= containerRect.left - cardRect.width &&
+			cardRect.left <= containerRect.right + cardRect.width &&
+			// right
+
+			cardRect.right <=
+				containerRect.right + cardRect.width &&
+			cardRect.right >= containerRect.left - cardRect.width;
+
+		console.log("Is card inside container?", isCardInsideContainer);
+
+		// write code to snap card vertical center of container
+		if (isCardInsideContainer) {
+			const containerCenterY =
+				containerRect.top + containerRect.height / 2;
+			const cardCenterY = cardRect.top + cardRect.height / 2;
+
+			const offsetYCenter = containerCenterY - cardCenterY;
+
+			top += offsetYCenter;
+		}
 	}
 </script>
 
-<section bind:this={container} class="bg-sky-100 w-96 h-16" />
+<section bind:this={container} class="relative m-16 p-16 bg-sky-100 w-96 h-8" />
+
+<!-- This is card -->
 <section
 	bind:this={card}
 	on:mousedown={onMouseDown}
 	style="left: {left}px; top: {top}px;"
-	class="cursor-move select-none bg-red-300 absolute h-8 w-16 transition-transform ease-linear"
+	class={isCardInsideContainer ? cardStyle : cardStyle}
 >
 	<slot />
 </section>
